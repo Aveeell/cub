4b19ee035fb74b28a24af9a1109b3d4bb736fa03
Jimmie Errok <jerrok@at-m7.msk.21-school.ru>
2022-06-22 16:43:51 +0300

    add check for spaces and zeros

--- check_wall.c
+++ check_wall.c
@@ -1,93 +1,5 @@
 #include "cub.h"
 
-/*
-static int check_spaces_above(char **map, int i, int j)
-{
-	if(i > 0)
-	{
-		while(map[i][j])
-		{
-		if(map[i][j] == ' ' && (ft_strlen(map[i]) == ft_strlen(map[i - 1])))
-			if(map[i - 1][j] != ' ' && map[i - 1][j] != '1')
-			{
-				printf("4.1\n");
-				error(map, i, j);
-			}
-		if(map[i][j] == '0' || map[i][j] == 'N' || map[i][j] == 'S' || 
-			map[i][j] == 'W' || map[i][j] == 'E')
-			if(map[i - 1][j] == ' ')
-				{
-				printf("4.2\n");
-				error(map, i, j);
-			}
-			j++;
-		}
-	}
-	return 0;
-}
-
-static int check_long_wall(char **map, int i)
-{
-	int j;
-	
-	j = ft_strlen(map[i - 1]);
-	if(i > 0 && ft_strlen(map[i]) > ft_strlen(map[i - 1]))
-	{
-		if(map[i][ft_strlen(map[i - 1]) - 1] == '1')
-		{
-			while(map[i][j])
-			{
-				if(map[i][j] != '1' && map[i][j] != ' ')
-				{
-					printf("2\n");
-					error(map, i, j);
-				}
-				j++;
-			}
-		}
-	}
-	return 0;
-}
-
-static int check_corner(char **map, int i, int j)
-{
-	j++;
-	if(i == 0)
-	{
-		if(map[i][j] == '0')
-			if(map[i - 1][j - 1] != '1')
-				error(map, i, j);
-		j = ft_strlen(map[i]) - 2;
-		if(map[i][j] == '0')
-			if(map[i - 1][j + 1] != '1')
-				error(map, i, j);
-	}
-	return 0;
-}
-
-static int check_wall_and_space(char **map, int i)
-{
-	int j;
-
-	j = 0;
-	check_long_wall(map, i);
-	check_spaces_above(map, i, j);
-	while(map[i][j])
-	{
-		while(map[i][j] == ' ')
-			j++;
-		check_corner(map, i, j);
-		if(ft_strlen(map[i]) > j && (map[i][j - 1] == ' ' && map[i][j] != '1'))
-		{
-			printf("1\n");
-			error(map, i, j);
-		}
-		j++;
-	}
-	return 0;
-}
-*/
-
 void check_with_prev_string(char **map, int i)
 {
 	int j;
@@ -100,7 +12,7 @@ void check_with_prev_string(char **map, int i)
 		if(j > k)
 		{
 			if(map[i][k] != '1')
-				error(map, i, j, "check_with_prev | no wall in pos");
+				error(map, i, j, "check_with_prev | no wall bottom last symbol from prev string");
 			while(j > k)
 			{
 				if(map[i][j] != ' ' && map[i][j] != '1')
@@ -159,6 +71,18 @@ void check_up_n_down(char **map)
 	}
 }
 
+int is_full_space(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] == ' ')
+		i++;
+	if(i == ft_strlen(str))
+		return 1;
+	return 0;
+}
+
 void check_walls_around_space(char **map)
 {
 	int i;
@@ -174,13 +98,39 @@ void check_walls_around_space(char **map)
 			{
 				if(j < ft_strlen(map[i - 1]) && (map[i - 1][j] != ' ' && map[i - 1][j] != '1'))
 					error(map, i, j, "check_space above");
-				if(j > 0 && (map[i][j - 1] != ' ' && map[i][j - 1] != '1'))
+				if(!is_full_space(map[i]) && j > 0 && (map[i][j - 1] != ' ' && map[i][j - 1] != '1'))
 					error(map, i, j, "check_space left");
 				if(j < ft_strlen(map[i + 1]) && (map[i + 1][j] != ' ' && map[i + 1][j] != '1'))
 					error(map, i, j, "check_space bottom");
-				if(j < ft_strlen(map[i]) && (map[i][j + 1] != ' ' && map[i][j + 1] != '1'))
+				if(!is_full_space(map[i]) && j < ft_strlen(map[i]) && (map[i][j + 1] != ' ' && map[i][j + 1] != '1'))
 					error(map, i, j, "check_space right");
 			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void check_all_zeros(char **map)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while(map[i])
+	{
+		j = 1;
+		while(map[i][j])
+		{
+			if (map[i][j] == '0' || map[i][j] == 'N' || map[i][j] == 'S' ||
+				map[i][j] == 'W' || map[i][j] == 'E')
+				{
+					if ((map[i - 1][j] == ' ' || !map[i - 1][j]) ||
+						(map[i + 1][j] == ' ' || !map[i + 1][j]) ||
+						(map[i][j - 1] == ' ' || !map[i][j - 1]) ||
+						(map[i][j + 1] == ' ' || !map[i][j + 1]))
+						error(map,i,j,"check_zeros");
+				}
 			j++;
 		}
 		i++;
@@ -192,5 +142,6 @@ int check_wall(char **map)
 	check_up_n_down(map);
 	check_wall_around_map(map);
 	check_walls_around_space(map);
+	check_all_zeros(map);
 	return 0;
 }
